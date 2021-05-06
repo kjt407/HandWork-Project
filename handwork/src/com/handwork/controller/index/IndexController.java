4e9bbb8d3a62e87b5adb9ec1f76c8dba91835565
kjt407 <kjt40700@gmail.com>
2021-05-06 14:33:25 +0900

    index page Context Edit

--- handwork/src/com/handwork/controller/index/IndexController.java
+++ handwork/src/com/handwork/controller/index/IndexController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/index")
+@WebServlet("/")
 public class IndexController extends HttpServlet {
 
 	@Override
