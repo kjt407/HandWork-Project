9ae7c3b6d86cf49cfd6326eb0e312882a4d48ebe
kjt407 <kjt40700@gmail.com>
2021-05-25 17:21:11 +0900

    mypage layout working

--- handwork/web/js/mypage.js
+++ handwork/web/js/mypage.js
@@ -37,13 +37,10 @@ function ajaxFunc(json) {
         dataType:"json",
         data:json,
         success:function(data){
-            alert('AJAX 통신성공');
             if(json.op == 'board'){
                 createBoard(data);
-                console.log(data);
             } else if( json.op == 'reply') {
                 createReply(data);
-                console.log(data);
             }
         },
         error:function(){
@@ -57,7 +54,8 @@ function createBoard(data) {
         const idx = data.list[i].boardnum;
         const type = data.list[i].boardname;
         const title = data.list[i].title;
-        const regdate = data.list[i].regdate;
+        let regdate = data.list[i].regdate;
+        regdate = regdate.substring(0,regdate.length-3);
         let typeKor;
         if (type == 'market'){
             typeKor = "수제공방";
@@ -85,30 +83,36 @@ function createBoard(data) {
 
 function createReply(data) {
     for(let i=0; i<data.list.length; i++){
-        const idx = data.list[i].boardnum;
-        const type = data.list[i].boardname;
-        const title = data.list[i].title;
-        const regdate = data.list[i].regdate;
+        const idx = data.list[i].board_num;
+        const type = data.list[i].replyname;
+        const content = data.list[i].content;
+        let regdate = data.list[i].regdate;
+        regdate = regdate.substring(0,regdate.length-3);
         let typeKor;
+        let typeBoard;
         if (type == 'review'){
             typeKor = "리뷰";
+            typeBoard = 'market';
         }else if (type == 'comment'){
             typeKor = "댓글";
+            typeBoard = 'request';
         }
 
-        $('ul.board-content-list.request').append(
+        $('ul.log-ul.reply').append(
             '<li class="log-li reply">\n' +
-            '                            <a href="">\n' +
-            '                                <p class="log-li-type reply">제작의뢰</p>\n' +
-            '                                <p class="log-li-title reply">제목입니다</p>\n' +
-            '                                <p class="log-li-date reply">제목입니다</p>\n' +
+            '                            <a href="'+getContextPath()+'/'+typeBoard+'/detail?id='+idx+'">\n' +
+            '                                <div class="wrap">\n' +
+            '                                   <p class="log-li-type '+type+' reply">'+typeKor+'</p>\n' +
+            '                                   <p class="log-li-title reply">'+content+'</p>\n' +
+            '                                </div>\n' +
+            '                                <p class="log-li-date reply">'+regdate+'</p>\n' +
             '                            </a>\n' +
             '                        </li>'
         )
     }
-    $('ul.board-content-list.request').next('#btn-request-more').remove();
+    $('ul.log-ul.reply').next('#btn-more-reply').remove();
     if(data.next){
-        $('ul.board-content-list.request').after('<input type="button" id="btn-request-more" class="btn-more" onclick="btnMoreReply()" value="↓">');
+        $('ul.log-ul.reply').after('<input type="button" id="btn-more-reply" class="btn-more" onclick="btnMoreReply()" value="↓">');
     }
 }
 function btnMoreBoard(){
